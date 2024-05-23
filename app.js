07cab49fa7b71575abc53670959e80e6d68f5988
Hamza Ghaffar <hamza@monke-labs.xyz>
2024-05-23 16:52:39 +0500

    Making portfolio

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 (function () {
-    [...document.querySelectorAll(".control")].forEach(button => {
-        button.addEventListener("click", function() {
-            document.querySelector(".active-btn").classList.remove("active-btn");
-            this.classList.add("active-btn");
-            document.querySelector(".active").classList.remove("active");
-            document.getElementById(button.dataset.id).classList.add("active");
-        })
+  [...document.querySelectorAll('.control')].forEach((button) => {
+    button.addEventListener('click', function () {
+      document.querySelector('.active-btn').classList.remove('active-btn');
+      this.classList.add('active-btn');
+      document.querySelector('.active').classList.remove('active');
+      document.getElementById(button.dataset.id).classList.add('active');
     });
-    document.querySelector(".theme-btn").addEventListener("click", () => {
-        document.body.classList.toggle("light-mode");
-    })
+  });
+  document.querySelector('.theme-btn').addEventListener('click', () => {
+    document.body.classList.toggle('light-mode');
+  });
 })();
+
+function submit() {
+  alert('I will reach you soon.');
+}
